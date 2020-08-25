084d8a96538bef0c12cfc038d5a2d2ef3b64198e
Rafinha Ferreira <rafael.ferreira@br.experian.com>
2020-08-25 18:57:10 -0300

    Add dependencies of Retrofit and a list of objects in json

---
+++ application/app/src/main/java/com/linkpocket/ui/LinkKeeperAdapter.kt
@@ -0,0 +1,24 @@
+package com.linkpocket.ui
+
+import android.view.View
+import com.bumptech.glide.Glide
+import com.domain.model.Preview
+import com.linkpocket.R
+import com.linkpocket.ext.ItemViewBuilder
+import kotlinx.android.synthetic.main.item_card_link.view.*
+
+class LinkKeeperAdapter : ItemViewBuilder<Preview>() {
+
+    override val layout: Int = R.layout.item_card_link
+
+    override fun View.onBind(position: Int) {
+        val item = collection.elementAt(position)
+        title.text = item.name
+        description.text = item.description
+
+        Glide.with(this)
+            .load(item.image)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(photo);
+    }
+}
